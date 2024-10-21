72a8240b0f15a6e35085934c4a4591fde5ea582a
kivisild <siimmarten.schmidt@gmail.com>
2024-10-21 13:37:30 +0300

    Add text file and boxen

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-import common from './common.js';
+/* import common from './common.js';
 common.hello();
 // kasuta es moduleid
 // ara common.js niiv2ga kasuta
 import es from './es.js';
 es.hello();
+ */
+import boxen from 'boxen';
+console.log(boxen('Unicorn'));
