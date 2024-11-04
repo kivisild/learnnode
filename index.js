1224970d12f3f486c6240fc987a279f0b71850ab
kivisild <siimmarten.schmidt@gmail.com>
2024-11-04 18:40:22 +0200

    04.11.24 tund (poolik)

--- index.js
+++
@@ -1,9 +0,0 @@
-/* import common from './common.js';
-common.hello();
-// kasuta es moduleid
-// ara common.js niiv2ga kasuta
-import es from './es.js';
-es.hello();
- */
-import boxen from 'boxen';
-console.log(boxen('Unicorn'));
