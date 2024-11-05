2d5cdde0b4604be87c77ac784f9c54eaff6f45a3
kivisild <siimmarten.schmidt@gmail.com>
2024-11-05 18:15:58 +0200

    04.11.24 lesson. Added html, css, and server.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,2 @@
-/* import common from './common.js';
-common.hello();
-// kasuta es moduleid
-// ara common.js niiv2ga kasuta
-import es from './es.js';
-es.hello();
- */
-// import boxen from 'boxen';
-// console.log(boxen('Unicorn'));
-
 import './style.css';
-console.log("Hello webpack");
+console.log("Hello webpack!");
