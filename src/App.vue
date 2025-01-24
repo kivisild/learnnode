dde3d7c2eb7fec603d16d8bf1fbdcb8bd906f101
kivisild <siimmarten.schmidt@gmail.com>
2025-01-24 20:33:24 +0200

    DRY - reduced repeated code. Added done items and todo items.

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,25 @@
 <script setup>
-import { ref } from 'vue';
+import { computed, ref } from 'vue';
+import ItemList from './itemList.vue';
 let message = ref('');
-let items = ref(["Piim", "Komm", "Pomm", "Plomm"])
+let i = 1;
+let items = ref([
+    { id: i++, name: "Piim", isDone: true },
+    { id: i++, name: "Komm", isDone: true },
+    { id: i++, name: "Pomm", isDone: false },
+    { id: i++, name: "Plomm", isDone: false },
+])
 
 function addItem(){
     if(message.value.trim() !== ''){
-        items.value.push(message.value.trim())
+        items.value.push({ id: i++, name: message.value.trim(), isDone: false });
     }
     message.value = '';
 }
+
+let doneItems = computed(() => items.value.filter(item => item.isDone));
+let toDoItems = computed(() => items.value.filter(item => !item.isDone));
+
 </script>
 
 <template>
@@ -24,12 +35,10 @@ function addItem(){
             </div>
         </div>
         <div class="content">
-            <h3>All items</h3>
-            <ul>
-            <li v-for="item in items">{{item}}</li>
-            </ul>
+            <ItemList :items="items" title="All items"></ItemList>
+            <ItemList :items="doneItems" title="Done items"></ItemList>
+            <ItemList :items="toDoItems" title="ToDo items"></ItemList>
         </div>
-        
     </div>
 
 </template>
