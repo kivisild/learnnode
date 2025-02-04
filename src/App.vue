c860f951f12e3c1f8e4a0bb8eb7effaac4e956c1
kivisild <siimmarten.schmidt@gmail.com>
2025-02-04 11:40:14 +0200

    Beginning with tabs

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,9 @@
 <script setup>
-import { ref } from 'vue';
-import Modal from './components/Modal.vue';
-import imgModal from './components/imgModal.vue'
-
-let modalActive = ref(false);
-let modalActive2 = ref(false);
-let modalActive3 = ref(false);
-
+import Tabs from './components/Tabs.vue';
+let titles = ['item1', 'item2', 'item3', 'item4']
 
 </script>
 
 <template>
-    <div class="container">
-        <section class="section">
-            <div class="buttons">
-                <button class="button is-primary" @click="modalActive=true">Modal Activate</button>
-                <button class="button is-warning is-outlined" @click="modalActive2=true">Modal activate 2</button>
-            </div>
-            
-        </section>
-    </div>
-
-    <imgModal :active="modalActive" @close="modalActive=false" url="https://picsum.photos/200/300"></imgModal>
-    <imgModal :active="modalActive2" @close="modalActive2=false" url="https://picsum.photos/200/300"></imgModal>
-
-
-
+    <Tabs :items="titles"></Tabs>
 </template>
-
-<style></style>
---
+++ src/pages/Modals.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { ref } from 'vue';
+import Modal from './components/Modal.vue';
+import imgModal from './components/imgModal.vue'
+
+let modalActive = ref(false);
+let modalActive2 = ref(false);
+let modalActive3 = ref(false);
+
+
+</script>
+
+<template>
+    <div class="container">
+        <section class="section">
+            <div class="buttons">
+                <button class="button is-primary" @click="modalActive=true">Modal Activate</button>
+                <button class="button is-warning is-outlined" @click="modalActive2=true">Modal activate 2</button>
+            </div>
+            
+        </section>
+    </div>
+
+    <imgModal :active="modalActive" @close="modalActive=false" url="https://picsum.photos/200/300"></imgModal>
+    <imgModal :active="modalActive2" @close="modalActive2=false" url="https://picsum.photos/200/300"></imgModal>
+
+
+
+</template>
+
+<style></style>
